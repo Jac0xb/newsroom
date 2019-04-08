3babe9fbbac06eb1c358b69fd257c82898eb7645
Connor Kuhn <connor.kuhn@kuhnclan.com>
2019-04-08 17:16:27 -0600

    Ignoring dist

--- document-api/dist/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.register = (app) => {
-    app.get("/", (req, res) => {
-        console.info("GET request recieved");
-        return res.send("OK");
-    });
-};
-//# sourceMappingURL=index.js.map
