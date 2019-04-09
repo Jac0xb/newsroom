08443e0b99c8f5451bd8f8ba9e5822d46f1c1e97
Connor Kuhn <connor.kuhn@kuhnclan.com>
2019-04-08 21:13:59 -0600

    Changing module decl

--- documentation/src/index.ts
+++ documentation/src/index.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import "dotenv/config";
 import express from "express";
-import swaggerCombine from "./swagger-combine";
+import swaggerCombine from "swagger-combine";
 import swaggerUi from "swagger-ui-express";
 
 dotenv.config();
