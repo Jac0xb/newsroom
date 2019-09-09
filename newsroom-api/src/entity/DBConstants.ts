01547821bafcab3667bd2ff2481b3f6ac62a1506
Connor Kuhn <connor.kuhn@kuhnclan.com>
2019-09-09 11:56:48 -0600

    Convert namespace to class

---
+++ newsroom-api/src/entity/DBConstants.ts
@@ -0,0 +1,12 @@
+export class DBConstants {
+    public static readonly READ = 0;
+    public static readonly WRITE = 1;
+    public static readonly USER_TABLE = "user";
+    public static readonly STGE_TABLE = "stage";
+    public static readonly ROLE_TABLE = "role";
+    public static readonly DOCU_TABLE = "document";
+    public static readonly WRKF_TABLE = "workflow";
+    public static readonly WFPERM_TABLE = "wfpermission";
+    public static readonly STPERM_TABLE = "stpermission";
+    public static readonly DCPERM_TABLE = "dcpermission";
+}
