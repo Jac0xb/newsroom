013255773ebfd844fddb862c67c0a5f81cdf94a6
Connor Kuhn <connor.kuhn@kuhnclan.com>
2019-09-11 21:03:35 -0600

    Adding Passportjs Google Oauth

---
+++ newsroom-api/src/middleware/GoogleOAuth2Provider.ts
@@ -0,0 +1,60 @@
+import { Express } from "express";
+import { authenticate, use } from "passport";
+import { OAuth2Strategy } from "passport-google-oauth";
+import { Service } from "typedi";
+import { Repository } from "typeorm";
+import { InjectRepository } from "typeorm-typedi-extensions";
+import { NRUser } from "../entity";
+
+/**
+ * Configures passportjs's Google OAuth2 provider
+ */
+@Service()
+export class GoogleOAuth2Provider {
+    private static readonly CALLBACK_URL = "/auth/google/callback";
+
+    @InjectRepository(NRUser)
+    private userRepository: Repository<NRUser>;
+
+    public configure(app: Express) {
+        // TODO Put keys in config
+        const strategy = new OAuth2Strategy({
+                callbackURL: GoogleOAuth2Provider.CALLBACK_URL,
+                clientID: "153384745741-7h66ureoaag1j61ei5u6un0faeh4al5h.apps.googleusercontent.com",
+                clientSecret: "u5Q2m0D1MO4DeulU-hCCHG06",
+            },
+            async (accessToken, refreshToken, profile, done) => {
+                const email = profile.emails[0].value;
+
+                const userName = email.split("@")[0];
+
+                let user = await this.userRepository.findOne({userName});
+
+                if (user == null) {
+                    user = this.userRepository.create();
+                    user.email = email;
+                    user.userName = userName;
+                    user.firstName = profile.name.givenName;
+                    user.lastName = profile.name.familyName;
+                    await this.userRepository.save(user);
+                }
+
+                done(null, user);
+            });
+
+        use(strategy);
+
+        app.get("/auth/google",
+            authenticate("google", {
+                scope: [
+                    "https://www.googleapis.com/auth/userinfo.profile",
+                    "https://www.googleapis.com/auth/userinfo.email",
+                ],
+            }));
+
+        app.get(GoogleOAuth2Provider.CALLBACK_URL,
+            authenticate("google", {failureRedirect: "/login"}), function(req, res) {
+                res.redirect("/");
+            });
+    }
+}
